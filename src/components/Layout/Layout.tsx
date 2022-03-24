e9a20ccdf9c066d8cf0d7faa6fd9cabada4b864d
MANAS <manasrathore2342@gmail.com>
2022-03-24 21:59:23 +0530

    first

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar/Navbar";
 
 const Layout = () => {
   return (
     <div>
-      {/* <Navbar /> */}
       <Outlet />
     </div>
   );
